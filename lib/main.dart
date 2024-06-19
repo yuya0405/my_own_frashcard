6008c2782977ade08bf0bb15b6bc5b121544464c
Yuya Ohbayashi <yuya9445@gmail.com>
2024-06-19 16:54:43 +0900

    186

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_own_frashcard/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -12,8 +11,27 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        
+        child: Column(
+          children: <Widget>[
+            Expanded(child: Image.asset("assets/images/image_title.png")),
+            _titleText(),
+            //TODO 横線,
+            //TODO 確認テストをするボタン,
+            //TODO ラジオボタン,
+            //TODO 単語一覧を見るボタン
+            Text("powered by oh884U8 2024")
+          ]
+        )
       ),
+    );
+  }
+
+  Widget _titleText() {
+    return Column(
+      children: <Widget>[
+        Text("私だけの単語帳", style: TextStyle(fontSize: 40.0),),
+        Text("My Own Frashcard", style: TextStyle(fontSize: 24.0),),
+      ],
     );
   }
 }
